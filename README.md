5bd2c22633111b91d2590e069e76d36827d4e4fe
MatthisLarbo <matlarbodiere@gmail.com>
2020-04-17 15:20:40 +0200

    suppression anciens fichiers

--- README.md
+++
@@ -1,3 +0,0 @@
-# PJ_WEB_2020
-le projet piscine réalisé par MAtthis Larbodière, Pascal Chen, Yimou Zhang
-Hello
--- monCompteModif.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$(".img-circle").on("change", function() {
-  var fileName = $(this).val().split("\\").pop();
-  $(this).siblings(".img-circle").addClass("selected").html(fileName);
-});
-
-?>
