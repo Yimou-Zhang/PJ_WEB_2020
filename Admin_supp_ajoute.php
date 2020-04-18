be3e14600abbb695a42d0c3e43b67f09182b02c2
MatthisLarbo <matlarbodiere@gmail.com>
2020-04-18 11:45:14 +0200

    modif admin_supp_ajoute

--- Admin_supp_ajoute.php
+++ Admin_supp_ajoute.php
@@ -1,3 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Ajouter/Supprimer Vendeur</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+<!--Partie CSS-->
+<style>
+        
+        .container {
+            width: 100%;
+            color: whitesmoke;
+        }
+
+        body{
+            background-image: url(bois.png);
+            background-repeat: repeat;
+        }
+
+        .navbar {
+            margin-bottom: 0;
+            border-radius: 0;
+        }
+
+        footer {
+            padding: 0px;
+            margin-top: 0px;
+            border-top: 0px;
+            color: white;
+            background-color: rgb(34, 31, 31);
+            position:absolute;
+            bottom:0;
+            width: 100%;
+        }
+        .titre {
+            margin:auto;
+            display: block;
+            text-align: center;
+            margin-top: 5%;
+            color: whitesmoke;
+        }
+
+        .test {
+            margin-left: auto;
+            margin-right: auto;
+            width: 30%;
+            margin-bottom: 70px;
+        }
+
+        .well {
+            margin-left: auto;
+            margin-right: auto;
+            width: 30%;
+            margin-bottom: 70px;
+        }
+    </style>
+
+</head>
+
+<body>
+
 <?php
 
 session_start();
@@ -19,7 +85,7 @@ session_start();
             }
             $result = mysqli_query($db_handle, $sql);
             if (mysqli_num_rows($result) == 0) {
-                echo "Personne non trouvé"; 
+                echo "Personne non trouvée"; 
             }else {
                 while ($data = mysqli_fetch_assoc($result) ) {
                     $id = $data['idUtilisateur'];
@@ -50,118 +116,202 @@ session_start();
             }
             $result = mysqli_query($db_handle, $sql);
             if (mysqli_num_rows($result) != 0) {
-                echo "Personne déjà existant";
+                echo "Personne déjà existante";
             }else {
                 $sql = "INSERT INTO utilisateur(nom, Prenom, email, motDePasse, pseudo, photoProfil, type) VALUES ('$nom','$prenom','$email','$password','$pseudo','$photo','$fonction')";
                 $result = mysqli_query($db_handle, $sql);//On enregistre
             }
             }else {
-                echo "Champ non remplis";
+                echo "Champ non rempli";
             }
 
         }
+        ?>
+
+
+
+
+
+        <!-- On passe sur du HTML pour remplir un formulaire de demande Ajouter ou Supprimer -->
+    
+        <!--Titre-->
+        <div class="titre">
+            <h2>
+            <span class="glyphicon glyphicon-transfer"></span> Ajouter/Supprimer un Vendeur
+            </h2>
+        </div>
+        
+        <!--Formulaire HTML-->
+        <div class="test" style="margin-top:4%">
+        <form action="Admin_supp_ajoute.php" method="post" class="contain" enctype='multipart/form-data'>
+                <div class="form-group row" style="margin-top:10px">
+                    <div class="col-sm-6" style="text-align:center">
+                        <button type="submit" class="btn btn-danger" name="supprime">Supprimer</button>
+                    </div>
+                    <div class="col-sm-6" style="text-align:center">
+                        <button type="submit" class="btn btn-success" name="ajoute">&nbsp&nbspAjouter&nbsp&nbsp</button> <!--&nbsp conserve les espaces (pour que le bouton soit de la meme forme que Suppr-->
+                    </div>
+                </div>
+                
+            </form>
+        </div>
+    
+            <!--PHP-->
+            <?php 
+        if(isset($_POST["ajoute"])){ //Les conditions ne fonctionnent qu'avec du php puis on remplit avec un formulaire pour l'email a ajouter
+            ?>
+
+
+        <!--HTML-->
+            <!-- Formulaire d'inscription pour ajouter un vendeur -->
+            
+        <div class="well">
+        <form action="Admin_supp_ajoute.php" method="post" class="contain" enctype='multipart/form-data' >
+            <div class="form-group row">
+                <label for="nom" class="col-sm-2 col-form-label">Nom</label>
+                <div class="col-sm-10">
+                    <input class="form-control" type="text" name="nom">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="prenom" class="col-sm-2 col-form-label">Prénom</label>
+                <div class="col-sm-10">
+                    <input class="form-control" type="text" name="prenom">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="email" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-10">
+                    <input class="form-control" type="email" name="email">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="pseudo" class="col-sm-2 col-form-label">Pseudo</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" name="pseudo">
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <label for="password" class="col-sm-2 col-form-label">Password</label>
+                <div class="col-sm-5" style="margin-left:5px">
+                    <input type="password" class="form-control" name="password">
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <label for="fonction" class="col-sm-2 col-form-label">Fonction</label>
+                <div class="col-sm-5">
+                    <select name="fonction">
+                        <option value="vendeur">Vendeur</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <label for="photo" class="col-sm-2 col-form-label">Photo de Profil</label>
+                <a class="btn btn-light" role="button">
+                    <input type="file" name="photo">
+                </a>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-6" style="text-align:center">
+                    <input type="submit" class="btn btn-light" name="non" value="Retour" style="border:solid">
+                </div>
+                <div class="col-sm-6" style="text-align:center">
+                    <input type="submit" class="btn btn-success" name="oui_ajoute" value="Ajouter ce Vendeur">
+                </div>
+            </div>
+        </form>
+    </div>
+    
+
+
+        <!--PHP-->
+        <?php 
+        }
+        if(isset($_POST["supprime"])){ //Ici un formulaire pour supprimer et il suffit de taper le mail du vendeur
+        ?>
+
+
+        <!--HTML-->
+
+        <div class="well" style="margin-top:5%">
+            <form action="Admin_supp_ajoute.php" method="post" class="contain" enctype='multipart/form-data' >
+                <div class="form-group row" style="margin-top:20px">
+                    <label for="t_mail" class="col-sm-3 col-form-label">Email</label>
+                    <div class="col-sm-9">
+                        <input class="form-control" type="email" name="t_mail">
+                    </div>
+                </div>
+                <div class="form-group row">
+            <div class="col-sm-6" style="text-align:center">
+                    <input type="submit" class="btn btn-light" name="oui" value="Retour" style="border:solid">
+                </div>
+                <div class="col-sm-6" style="text-align:center">
+                    <input type="submit" class="btn btn-danger" name="oui_supprime" value="Supprimer ce Vendeur">
+                </div>
+            </div>
+            </form>
+        </div>
+
+<?php
+        }
+//php
 ////////////Boucle de recherche d'identification pour l'acheteur
         $sql = "SELECT * FROM utilisateur WHERE type LIKE 'vendeur'";
         
         $result = mysqli_query($db_handle, $sql);
+        ?> 
 
-        if (mysqli_num_rows($result) == 0) {
-            echo "Bug";
-        } else {             
+        <!--Affichage "Liste Vendeurs" en HTML-->
+        <div class="titre">
+            <h3>
+                <span class="glyphicon glyphicon-list"></span> Liste des Vendeurs
+            </h3>
+        </div>
+
+        <?php if (mysqli_num_rows($result) == 0) {
+?>
+
+<!--Affichage aucun vendeur dans la liste-->
+<div class="titre"style="margin-top:20px">
+    <h5>
+        <span class="glyphicon glyphicon-exclamation-sign"></span> Aucun Vendeur dans la liste
+    </h5>
+</div>
+
+<!--PHP-->
+<?php
+        ;} else {             
             echo "<table border='1'>";
-            while($data = mysqli_fetch_assoc($result)){ 
-                echo "<tr>";
-                $image = $data['photoProfil'];
-                echo "<td>" . "<img src='$image' height='120' width='100'>" . "</td>";
-                echo "<td>". "<th>" . "Nom :" . " ". $data['nom'] . "</th>" . "</td>";
-                echo "<td>". "<th>" . "Prenom :". " " . $data['prenom']. "</th>"  . "</td>";
-                echo "<td>"."<th>" . "Email :". " " . $data['email'] . "</th>" . "</td>";
-                echo "</tr>";
-            }
+            while($data = mysqli_fetch_assoc($result)){   
+
+?>
+
+<!--HTML-->
+
+<div class="row">
+            <div class="well" style="width:50%">
+                <div class="row" style="margin-top:0px">
+                    <div class="col-sm-3">
+                        <img src="<?php  $image = $data['photoProfil']; ?>" class="img-responsive" style="width:100%" alt="Photo de Profil">
+                    </div>
+                    <div class="col-sm-4">
+                        <p><strong>Nom : </strong></p> <?php echo $data['nom']; ?> <!--Affichage Prénom-->
+                        <p><strong>Prénom : </strong></p> <?php echo $data['prenom']; ?>
+                    </div>
+                    <div class="col-sm-5">
+                        <p><strong>Email : </strong></p> <?php echo $data['email'] ?>
+                        <p><strong>Pseudo : </strong></p> <?php echo $data['pseudo'] ?>
+                    </div>
+                </div>
+            </div>
+</div>
+
+<?php
+}
             echo "</table>";
-        }
-        ?>
-        <!-- On passe sur du HTML pour remplir un formulaire de demande Ajouter ou Supprimer -->
-        <form action="Admin_supp_ajoute.php" method="post" class="contain" enctype='multipart/form-data' >
-        <table>
-            <tr>
-                <td colspan="2" align="center">
-                    <br>
-                    <input type="submit" name="ajoute" value="Ajouter">
-                    <input type="submit" name="supprime" value="Supprimer">
-                </td>
-            </tr>
-        </table>
-        </form>
-        <?php
-        if(isset($_POST["ajoute"])){ //Les conditions ne fonctionnent qu'avec du php puis on remplit avec un formulaire pour l'email a ajouter
-            ?>
-            <!-- Formulaire d'inscription pour ajouter un vendeur -->
-            <form action="Admin_supp_ajoute.php" method="post" class="contain" enctype='multipart/form-data' >
-                <table>
-                <tr>
-                    <td>Nom:</td>
-                    <td><input type="text" name="nom"></td>
-                </tr>
-                <tr>
-                    <td>Prenom:</td>
-                    <td><input type="text" name="prenom"></td>
-                </tr>
-                <tr>
-                    <td>Email:</td>
-                    <td><input type="text" name="email"></td>
-                </tr>
-                <tr>
-                    <td>Pseudo:</td>
-                    <td><input type="text" name="pseudo"></td>
-                </tr>
-                <tr>
-                    <td>Mot de passe:</td>
-                    <td><input type="password" name="password"></td>
-                </tr>
-                <tr>
-                    <td>Fonction:</td>
-                    <td><select name="fonction">
-                        <option value="vendeur">Vendeur </option>
-                        </select>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Photo de profil :</td>
-                    <td><input type="file" name="photo"></td>
-                </tr>
-                <tr>
-                    <tr>
-                        <td colspan="2" align="center">
-                            <input type="submit" name="oui_ajoute" value="Ajoute cette personne">
-                            <input type="submit" name="non" value="Retour">
-                        </td>
-                    </tr>
-                </table>            
-            </form>
-            <?php 
-        }
-        if(isset($_POST["supprime"])){ //Ici un formulaire pour supprimer et il suffit de taper le mail du vendeur
-        ?>
-<<<<<<< HEAD
-        <form action="Admin_supp_ajoute" method="post" class="contain" enctype='multipart/form-data' >
-=======
-        <form action="Admin_supp_ajoute.php" method="post" class="contain" enctype='multipart/form-data' >
->>>>>>> e5985a02fd798197d39eb4defe02d91f96640c1a
-            <table>
-                <tr>
-                    <td>Email:</td>
-                    <td><input type="text" name="t_mail"></td>
-                </tr>
-                <tr>
-                    <td colspan="2" align="center">
-                        <input type="submit" name="oui_supprime" value="Supprime cette personne">
-                        <input type="submit" name="non" value="Retour">
-                    </td>
-                </tr>
-            </table>            
-        </form>
-        <?php  
         }
     }
     else{
@@ -169,3 +319,12 @@ session_start();
     }
 
 ?>
+
+
+<!--
+    <footer class="container-fluid text-center">
+            <p>Site designé par Yimou ZHANG, Pascal CHEN et Matthis LARBODIERE</p>
+    </footer>
+-->
+</body>
+</html>
