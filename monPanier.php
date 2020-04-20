ddc9f7cc575858e7aae46c78233c52d130c817e0
MatthisLarbo <matlarbodiere@gmail.com>
2020-04-20 15:39:19 +0200

    titre pages

--- monPanier.php
+++ monPanier.php
@@ -44,7 +44,7 @@
 <html lang="en">
 
 <head>
-    <title>Accueil</title>
+    <title>Mon Panier</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
--- pageItem.php
+++ pageItem.php
@@ -32,7 +32,7 @@ if($db_found){
 <html lang="en">
 
 <head>
-    <title>Accueil</title>
+    <title>Page Item</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
